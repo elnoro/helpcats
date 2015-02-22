0f5bbb7d23d632087cb4287f04b353f1d9b9e0dd
elnoro <karimov.work@gmail.com>
2015-02-23 01:29:18 +0500

    update view and controller to show cats in rows

--- web/js/app.js
+++ web/js/app.js
@@ -5,20 +5,60 @@ helpCatsApp.controller('catsCtrl', function ($scope) {
 		{
 			"name": "Киска 1",
 			"image": "https://pp.vk.me/c621316/v621316464/15ca5/jYoWFiQEKYg.jpg",
-			"shortDescription": "Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.",
+			"shortDescription": "Милейший молодой котик ищет себе новый дом. На вид коту года три. Ласковый,к рукам приучен,мурлычет,нежнейшее создание,ходит в лоток. По всем подробностям обращаться по ",
+			"link": "#"
+		},
+		{
+			"name": "Киска 2",
+			"image": "https://pp.vk.me/c621830/v621830713/ebfa/hzo4z-8BxJs.jpg",
+			"shortDescription": "В районе Доватора найдена/подкинута вот такая вот девочка!!! Возраст около 5-6 месяцев! Ласковая, с необычайно мягкой и шелковистой шерсткой! Временно приютили в подъезде, но кошка очень сильно стрессует, ",
 			"link": "#"
 		},
 		{
 			"name": "Киска 3",
-			"image": "images/pic03.jpg",
-			"shortDescription": "Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.",
+			"image": "https://pp.vk.me/c621316/v621316464/15ca5/jYoWFiQEKYg.jpg",
+			"shortDescription": "Милейший молодой котик ищет себе новый дом. На вид коту года три. Ласковый,к рукам приучен,мурлычет,нежнейшее создание,ходит в лоток. По всем подробностям обращаться по ",
 			"link": "#"
 		},
 		{
-			"name": "Киска 1",
-			"image": "images/pic02.jpg",
-			"shortDescription": "Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.",
+			"name": "Киска 4",
+			"image": "https://pp.vk.me/c621830/v621830713/ebfa/hzo4z-8BxJs.jpg",
+			"shortDescription": "В районе Доватора найдена/подкинута вот такая вот девочка!!! Возраст около 5-6 месяцев! Ласковая, с необычайно мягкой и шелковистой шерсткой! Временно приютили в подъезде, но кошка очень сильно стрессует, ",
 			"link": "#"
 		},
+		{
+			"name": "Киска 5",
+			"image": "https://pp.vk.me/c621316/v621316464/15ca5/jYoWFiQEKYg.jpg",
+			"shortDescription": "Милейший молодой котик ищет себе новый дом. На вид коту года три. Ласковый,к рукам приучен,мурлычет,нежнейшее создание,ходит в лоток. По всем подробностям обращаться по ",
+			"link": "#"
+		},
+		{
+			"name": "Киска 6",
+			"image": "https://pp.vk.me/c621830/v621830713/ebfa/hzo4z-8BxJs.jpg",
+			"shortDescription": "В районе Доватора найдена/подкинута вот такая вот девочка!!! Возраст около 5-6 месяцев! Ласковая, с необычайно мягкой и шелковистой шерсткой! Временно приютили в подъезде, но кошка очень сильно стрессует, ",
+			"link": "#"
+		},
+		{
+			"name": "Киска 7",
+			"image": "https://pp.vk.me/c621316/v621316464/15ca5/jYoWFiQEKYg.jpg",
+			"shortDescription": "Милейший молодой котик ищет себе новый дом. На вид коту года три. Ласковый,к рукам приучен,мурлычет,нежнейшее создание,ходит в лоток. По всем подробностям обращаться по ",
+			"link": "#"
+		},
+		{
+			"name": "Киска 8",
+			"image": "https://pp.vk.me/c621830/v621830713/ebfa/hzo4z-8BxJs.jpg",
+			"shortDescription": "В районе Доватора найдена/подкинута вот такая вот девочка!!! Возраст около 5-6 месяцев! Ласковая, с необычайно мягкой и шелковистой шерсткой! Временно приютили в подъезде, но кошка очень сильно стрессует, ",
+			"link": "#"
+		}
 	];
+
+	var splitArrayIntoChunks = function (originalArray, chunkSize) {
+		var chunks = [];
+		for (var i = 0; i < originalArray.length; i += chunkSize) {
+		    chunks.push(originalArray.slice(i, i + chunkSize));
+		};
+		return chunks;
+	}
+
+	$scope.catChunks = splitArrayIntoChunks($scope.cats, 3);
 });
