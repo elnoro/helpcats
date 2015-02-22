d8cef9c80b0d829fea062a483ee9eaea7b2f093f
elnoro <karimov.work@gmail.com>
2015-02-23 03:48:00 +0500

    add db connection

--- app/config/db.php
+++ app/config/db.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=localhost;dbname=helpcats',
     'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'password' => '1',
+    'charset'  => 'utf8',
 ];
