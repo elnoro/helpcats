e277c1e6b2b96b4122a9307f77177399e08a2c33
elnoro <karimov.work@gmail.com>
2015-02-23 04:53:48 +0500

    update rest images

--- app/models/Cat.php
+++ app/models/Cat.php
@@ -5,4 +5,19 @@ use yii\db\ActiveRecord;
 
 class Cat extends ActiveRecord
 {
+	public function fields()
+	{
+		return array_merge(parent::fields(), ['images' => function ($model) {
+			$images = [];
+			foreach ($model->images as $catImage) {
+				$images[] = $catImage->url;
+			}
+			return $images;
+		}]);
+	}
+
+	public function getImages()
+	{
+		return $this->hasMany(CatImage::className(), ['cat_id' => 'id']);
+	}
 }
