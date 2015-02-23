c03a3f106b25b796a7e5ab12c3d4b39e98d38cea
elnoro <karimov.work@gmail.com>
2015-02-23 13:33:06 +0500

    * add CatSearchItem * add indexing

--- app/models/Cat.php
+++ app/models/Cat.php
@@ -25,4 +25,20 @@ class Cat extends ActiveRecord
 	{
 		return [['source', 'unique']];
 	}
+
+	public function afterSave()
+	{
+		if (is_null($item = CatSearchItem::get($this->id))) {
+			$item = new CatSearchItem;
+			$item->id = $this->id;
+		} 
+		$item->name = $this->name;
+		$item->description = $this->description;
+		$item->save();
+	}
+
+	public function afterDelete()
+	{
+		CatSearchItem::get($this->id)->delete();
+	}
 }
---
+++ app/models/CatSearchItem.php
@@ -0,0 +1,13 @@
+<?php 
+class CatSearchItem extends \yii\elasticsearch\ActiveRecord
+{
+    public function attributes()
+    {
+        return ['id', 'name', 'description'];
+    }
+
+    public function getCat()
+    {
+    	return Cat::findOne($this->id)
+    }
+}
